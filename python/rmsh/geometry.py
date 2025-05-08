bf42af34e3106144ac1b4e9688b77be0629f91cf
Jan Roth <r0th.j4n@gmail.com>
2025-05-08 14:50:50 +0200

    feat: rework boundaries
    Boundares are now easier to use. Curve boundaries now have many utility functions, which allow for creation from lines, splines, or any functions.
    Boundaries shared with blocks, they are now easier to use, with methods that allow for connecting blocks very easily.

--- python/rmsh/geometry.py
+++ python/rmsh/geometry.py
@@ -1,10 +1,15 @@
 """Geometrical primitive objects."""
 
+from __future__ import annotations
+
+from collections.abc import Callable
 from dataclasses import dataclass
 from enum import IntEnum, unique
+from typing import Self
 
 import numpy as np
 import numpy.typing as npt
+from scipy.special import comb
 
 INVALID_POINT_IDX = -1
 INVALID_LINE_IDX = 0
@@ -67,14 +72,50 @@ class BoundaryId(IntEnum):
         - BoundarySouth
     """
 
-    BoundarySouth = 1
-    BoundaryEast = 2
-    BoundaryNorth = 3
-    BoundaryWest = 4
+    BoundaryBottom = 1
+    BoundaryRight = 2
+    BoundaryTop = 3
+    BoundaryLeft = 4
+
+    @property
+    def opposite_boundary(self) -> BoundaryId:
+        """Return boundary opposite the specified one."""
+        return BoundaryId(((self.value + 1) & 3) + 1)
+
+    @property
+    def next(self) -> BoundaryId:
+        """Return the boundary which is the next in orientation."""
+        return BoundaryId((self.value & 3) + 1)
+
+    @property
+    def prev(self) -> BoundaryId:
+        """Return the boundary which is the previous in orientation."""
+        return BoundaryId(((self.value - 2) & 3) + 1)
 
 
 @dataclass(frozen=True)
-class BoundaryBlock:
+class BoundaryRef:
+    """Class which acts as the reference to a boundary."""
+
+    block: MeshBlock
+    boundary: BoundaryId
+
+
+@dataclass(frozen=True)
+class Boundary:
+    """Base class for boundaries.
+
+    Parameters
+    ----------
+    n : int
+        Number of points on the boundary.
+    """
+
+    n: int
+
+
+@dataclass(frozen=True)
+class BoundaryBlock(Boundary):
     """Defines a connection to a mesh block with the label `target`.
 
     Parameters
@@ -88,13 +129,11 @@ class BoundaryBlock:
         be inferred from other boundaries this one has to match to.
     """
 
-    target: str
-    target_id: BoundaryId
-    n: int = 0
+    ref: BoundaryRef
 
 
-@dataclass(frozen=True, init=False)
-class BoundaryCurve:
+@dataclass(frozen=True)
+class BoundaryCurve(Boundary):
     """Defines values of points along the boundary of a mesh block.
 
     Parameters
@@ -107,18 +146,145 @@ class BoundaryCurve:
 
     x: npt.NDArray[np.float64]
     y: npt.NDArray[np.float64]
-    n: int
 
     def __init__(self, x: npt.ArrayLike, y: npt.ArrayLike) -> None:
         xv = np.array(x, np.float64)
         yv = np.array(y, np.float64)
-        if xv.ndim != 1 or xv.size != yv.size or xv.size == 0:
+        if xv.ndim != 1 or yv.ndim != 1 or xv.size != yv.size or xv.size == 0:
             raise ValueError(
                 "Both x and y must be 1d arrays of the same (non-zero) length."
             )
         object.__setattr__(self, "x", xv)
         object.__setattr__(self, "y", yv)
-        object.__setattr__(self, "n", int(xv.size))
+        super().__init__(xv.size)
+
+    @classmethod
+    def from_samples(
+        cls,
+        func: Callable[[npt.NDArray[np.float64]], npt.ArrayLike],
+        n: int,
+        distribution: Callable[[npt.NDArray[np.float64]], npt.ArrayLike] | None = None,
+    ) -> Self:
+        """Create boundary based on samples of a callable.
+
+        Parameters
+        ----------
+        func : (array) -> array_like
+            Coordinates as function of fraction of distance along the curve.
+
+        n : int
+            Number of samples to take.
+
+        distribution : (array_like) -> array, optional
+            Function to transform the uniform samples into desired distribution.
+
+        Returns
+        -------
+        BoundaryCurve
+            Boundary which follows the specified function.
+        """
+        if n < 2:
+            raise ValueError(
+                f"Boundary must have at least 2 points, but {n=} were given."
+            )
+
+        sample_fractions = np.linspace(0, 1, n, dtype=np.float64)
+        if distribution is not None:
+            sample_fractions = np.asarray(
+                distribution(sample_fractions), dtype=np.float64, copy=None
+            )
+
+        # Don't copy if not necessary
+        coords = np.asarray(func(sample_fractions), np.float64, copy=None)
+        if coords.shape != (n, 2):
+            raise ValueError(
+                f"Function given did not map {n} input samples into n 2d vectors"
+                f" (shape {(n, 2)}), instead the resulting array had the shape"
+                f" {coords.shape}."
+            )
+
+        return cls(coords[:, 0], coords[:, 1])
+
+    @classmethod
+    def from_line(
+        cls,
+        begin: npt.ArrayLike,
+        end: npt.ArrayLike,
+        n: int,
+        distribution: Callable[[npt.NDArray[np.float64]], npt.ArrayLike] | None = None,
+    ) -> Self:
+        """Create boundary based on samples of a callable.
+
+        Parameters
+        ----------
+        begin : array_like
+            Coordinates of the start function.
+
+        end : array_like
+            Coordinates of the end function.
+
+        n : int
+            Number of samples to take.
+
+        distribution : (array_like) -> array, optional
+            Function to transform the uniform samples into desired distribution.
+
+        Returns
+        -------
+        BoundaryCurve
+            Boundary which follows the specified function.
+        """
+        p0 = np.asarray(begin, np.float64, copy=None).flatten()
+        p1 = np.asarray(end, np.float64, copy=None).flatten()
+        if p0.shape != (2,) or p1.shape != (2,):
+            raise ValueError("Both begin and end must be arrays with 2 elements.")
+        return cls.from_samples(
+            lambda s: p0[None, :] * (1 - s[:, None]) + p1[None, :] * s[:, None],
+            n,
+            distribution,
+        )
+
+    @classmethod
+    def from_knots(
+        cls,
+        n: int,
+        *knots: tuple[float, float] | npt.ArrayLike,
+        distribution: Callable[[npt.NDArray[np.float64]], npt.ArrayLike] | None = None,
+    ) -> Self:
+        """Create the boundary using Bernstein polynomials with specified knots.
+
+        Parameters
+        ----------
+        n : int
+            Number of samples to take.
+
+        *knots : (float, float) or array_like
+            Knots to use for Bernstein polynomials.
+
+        distribution : (array_like) -> array, optional
+            Function to transform the uniform samples into desired distribution.
+
+        Returns
+        -------
+        BoundaryCurve
+            Boundary which follows the specified function.
+        """
+        if len(knots) < 2:
+            raise ValueError("At least two knots are needed")
+
+        weights = np.array(knots, np.float64)
+        if weights.ndim != 2 or weights.shape[1] != 2:
+            raise ValueError("Each knot should contain two elements.")
+
+        pos = np.linspace(0, 1, n, dtype=np.float64)
+        if distribution is not None:
+            pos = np.asarray(distribution(pos), np.float64, copy=None)
+
+        nk = weights.shape[0]
+        k = np.arange(nk)
+        bern = comb(nk - 1, k) * pos[:, None] ** k * (1 - pos[:, None]) ** (nk - 1 - k)
+        coords = bern @ weights
+        return cls(coords[:, 0], coords[:, 1])
 
 
 _SolverCfgTuple = tuple[bool, float, int, int, int]
@@ -129,3 +295,98 @@ _BoundaryInfoTuple = (
 _BlockInfoTuple = tuple[
     str, _BoundaryInfoTuple, _BoundaryInfoTuple, _BoundaryInfoTuple, _BoundaryInfoTuple
 ]
+
+
+@dataclass
+class MeshBlock:
+    """Basic building block of the mesh, which describes a structured mesh block.
+
+    Boundaries of the block can be either prescribed curves, or instead a
+    "soft" boundary, where the only information specified is that it should connect to
+    another block. Such "soft" boundaries allow for a potentially smoother transition
+    between different blocks.
+
+    When blocks share a boundary, the only requirement is that the boundary that is
+    shared between them has such a number of nodes that the opposite boundary matches it.
+
+    Parameters
+    ----------
+    label : str
+            Label by which this block will be referred to in the mesh, as well as by other
+            blocks which have a block
+            boundary to this block.
+    boundaries : Mapping of BoundaryId to BoundaryCurve or BoundaryBlock
+        Dictionary containing the boundaries with their respective IDs. All four might be
+        specified, or only a few.
+    """
+
+    label: str | None = None
+    bottom: Boundary | None = None
+    right: Boundary | None = None
+    top: Boundary | None = None
+    left: Boundary | None = None
+
+    def has_all_boundaries(self) -> bool:
+        """Check if a mesh block has all four boundaries specified as non-None values.
+
+        Returns
+        -------
+        bool
+            True if the mesh block has all needed boundaries and False if that is not the
+            case.
+        """
+        return (
+            (self.top is not None)
+            and (self.bottom is not None)
+            and (self.left is not None)
+            and (self.right is not None)
+        )
+
+    def bbnd_top(self, n: int = 0) -> BoundaryBlock:
+        """Boundary to this block's boundary."""
+        return BoundaryBlock(n, BoundaryRef(self, BoundaryId.BoundaryTop))
+
+    def bbnd_bottom(self, n: int = 0) -> BoundaryBlock:
+        """Boundary to this block's boundary."""
+        return BoundaryBlock(n, BoundaryRef(self, BoundaryId.BoundaryBottom))
+
+    def bbnd_left(self, n: int = 0) -> BoundaryBlock:
+        """Boundary to this block's boundary."""
+        return BoundaryBlock(n, BoundaryRef(self, BoundaryId.BoundaryLeft))
+
+    def bbnd_right(self, n: int = 0) -> BoundaryBlock:
+        """Boundary to this block's boundary."""
+        return BoundaryBlock(n, BoundaryRef(self, BoundaryId.BoundaryRight))
+
+    def get_boundary_by_id(self, bnd_id: BoundaryId, /) -> Boundary | None:
+        """Return the boundary with the correct id."""
+        if bnd_id == BoundaryId.BoundaryBottom:
+            return self.bottom
+        if bnd_id == BoundaryId.BoundaryTop:
+            return self.top
+        if bnd_id == BoundaryId.BoundaryLeft:
+            return self.left
+        if bnd_id == BoundaryId.BoundaryRight:
+            return self.right
+
+        raise ValueError(f"Id {bnd_id} is not a valid value.")
+
+    def get_boundary_by_id_existing(self, bnd_id: BoundaryId, /) -> Boundary:
+        """Get a boundary for the corresponding ID and make sure it's not None."""
+        bnd = self.get_boundary_by_id(bnd_id)
+        if bnd is None:
+            raise ValueError(f"Block does not have a defined boundary for id {bnd_id}.")
+        return bnd
+
+    def set_boundary_by_id(self, bnd_id: BoundaryId, val: Boundary | None, /) -> None:
+        """Return the boundary with the correct id."""
+        if bnd_id == BoundaryId.BoundaryBottom:
+            self.bottom = val
+        elif bnd_id == BoundaryId.BoundaryTop:
+            self.top = val
+        elif bnd_id == BoundaryId.BoundaryLeft:
+            self.left = val
+        elif bnd_id == BoundaryId.BoundaryRight:
+            self.right = val
+        else:
+            raise ValueError(f"Id {bnd_id} is not a valid value.")
--- tests/test_harder.py
+++ tests/test_harder.py
@@ -3,17 +3,16 @@
 # import time
 
 import numpy as np
+import numpy.typing as npt
 
 # from matplotlib import collections as col
 # from matplotlib import pyplot as plt
 from rmsh import (
-    BoundaryBlock,
     BoundaryCurve,
-    BoundaryId,
-    MeshBlock,
     SolverConfig,
     create_elliptical_mesh,
 )
+from rmsh.geometry import MeshBlock
 
 
 def test_circular():
@@ -21,79 +20,58 @@ def test_circular():
     N1 = 80
     N2 = 70
     NR = 70
-    angle1 = np.linspace(0, np.pi / 2, N1)
-    angle2 = np.linspace(np.pi / 2, np.pi, N2)
-    angle3 = np.linspace(np.pi, 3 * np.pi / 2, N1)
-    angle4 = np.linspace(3 * np.pi / 2, 2 * np.pi, N2)
 
-    bnd1 = BoundaryCurve(np.cos(angle1), np.sin(angle1))
-    bnd2 = BoundaryCurve(np.cos(angle2), np.sin(angle2))
-    bnd3 = BoundaryCurve(np.cos(angle3), np.sin(angle3))
-    bnd4 = BoundaryCurve(np.cos(angle4), np.sin(angle4))
+    bc = MeshBlock("center")
+    bb = MeshBlock("bottom")
+    br = MeshBlock("right")
+    bt = MeshBlock("top")
+    bl = MeshBlock("left")
 
-    b1 = MeshBlock(
-        "right",
-        {
-            BoundaryId.BoundaryEast: bnd1,
-            BoundaryId.BoundaryNorth: BoundaryBlock("top", BoundaryId.BoundaryEast, NR),
-            BoundaryId.BoundarySouth: BoundaryBlock("btm", BoundaryId.BoundaryEast, NR),
-            BoundaryId.BoundaryWest: BoundaryBlock("center", BoundaryId.BoundaryEast),
-        },
+    def bnd_function(theta: npt.ArrayLike) -> npt.NDArray[np.float64]:
+        """Define the boundary with."""
+        return np.stack((np.cos(theta), np.sin(theta)), axis=1, dtype=np.float64)
+
+    br.right = BoundaryCurve.from_samples(bnd_function, N1, lambda x: (x + 1) * np.pi / 2)
+    br.left = bc.bbnd_right()
+    br.top = bt.bbnd_right(NR)
+    br.bottom = bb.bbnd_right()
+
+    bl.right = bc.bbnd_left()
+    bl.left = BoundaryCurve.from_samples(bnd_function, N1, lambda x: (x + 3) * np.pi / 2)
+    bl.top = bt.bbnd_left()
+    bl.bottom = bb.bbnd_left()
+
+    bt.right = br.bbnd_top()
+    bt.left = bl.bbnd_top()
+    bt.top = BoundaryCurve.from_samples(bnd_function, N2, lambda x: (x + 2) * np.pi / 2)
+    bt.bottom = bc.bbnd_top()
+
+    bb.right = br.bbnd_bottom()
+    bb.left = bl.bbnd_bottom()
+    bb.top = bc.bbnd_bottom()
+    bb.bottom = BoundaryCurve.from_samples(
+        bnd_function, N2, lambda x: (x + 0) * np.pi / 2
     )
 
-    b3 = MeshBlock(
-        "left",
-        {
-            BoundaryId.BoundaryWest: bnd3,
-            BoundaryId.BoundaryNorth: BoundaryBlock("top", BoundaryId.BoundaryWest),
-            BoundaryId.BoundarySouth: BoundaryBlock("btm", BoundaryId.BoundaryWest),
-            BoundaryId.BoundaryEast: BoundaryBlock("center", BoundaryId.BoundaryWest),
-        },
-    )
+    bc.right = br.bbnd_left()
+    bc.top = bt.bbnd_bottom()
+    bc.left = bl.bbnd_right()
+    bc.bottom = bb.bbnd_top()
 
-    b2 = MeshBlock(
-        "top",
-        {
-            BoundaryId.BoundaryWest: BoundaryBlock("left", BoundaryId.BoundaryNorth),
-            BoundaryId.BoundaryNorth: bnd2,
-            BoundaryId.BoundarySouth: BoundaryBlock("center", BoundaryId.BoundaryNorth),
-            BoundaryId.BoundaryEast: BoundaryBlock("right", BoundaryId.BoundaryNorth),
-        },
-    )
-
-    b4 = MeshBlock(
-        "btm",
-        {
-            BoundaryId.BoundaryWest: BoundaryBlock("left", BoundaryId.BoundarySouth),
-            BoundaryId.BoundaryNorth: BoundaryBlock("center", BoundaryId.BoundarySouth),
-            BoundaryId.BoundarySouth: bnd4,
-            BoundaryId.BoundaryEast: BoundaryBlock("right", BoundaryId.BoundarySouth),
-        },
-    )
-
-    b0 = MeshBlock(
-        "center",
-        {
-            BoundaryId.BoundaryWest: BoundaryBlock("left", BoundaryId.BoundaryEast),
-            BoundaryId.BoundaryEast: BoundaryBlock("right", BoundaryId.BoundaryWest),
-            BoundaryId.BoundaryNorth: BoundaryBlock("top", BoundaryId.BoundarySouth),
-            BoundaryId.BoundarySouth: BoundaryBlock("btm", BoundaryId.BoundaryNorth),
-        },
-    )
     # t0 = time.time_ns()
     cfg = SolverConfig(tolerance=1e-5, max_iterations=512, smoother_rounds=0)
     m, rx, ry = create_elliptical_mesh(
-        [b0, b1, b2, b3, b4], verbose=False, solver_cfg=cfg
+        [bc, br, bt, bl, bb], verbose=False, solver_cfg=cfg
     )
-    del m
+    # del m
     assert rx <= cfg.tolerance and ry <= cfg.tolerance
     # t1 = time.time_ns()
 
     # print("Time taken for the mesh generation:", (t1 - t0) / 1e9, "seconds")
     # print("Residuals:", rx, ry)
 
-    # x = m.x
-    # y = m.y
+    # x = m.pos_x
+    # y = m.pos_y
     # ln = m.lines
     # # lnvals = []
     # xb = x[ln[:, 0]]
